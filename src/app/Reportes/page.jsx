2557aefdcca1461acc7db11e7b95bb3ebc9a463a
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-03-05 13:23:13 -0600

    empiezo a crear las rutas para el back

--- src/app/Reportes/page.jsx
+++ src/app/Reportes/page.jsx
@@ -1,6 +1,8 @@
 //Aqui se tienen que hacer el despliegue de los reportes
 export default function Reportes() {
     return (
+        
         <div>Paginas de los reportes</div>
+        
     )
 }
