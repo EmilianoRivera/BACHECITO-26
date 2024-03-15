525e36ccf3d2ab2ae9289f8511d6381f85c163a3
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-03-15 01:41:39 -0600

    añado admin y consultas

--- src/app/Reportes/page.jsx
+++ src/app/Reportes/page.jsx
@@ -1,8 +1,49 @@
+"use client"
+import { useEffect, useState } from "react";
+
 //Aqui se tienen que hacer el despliegue de los reportes
-export default function Reportes() {
-    return (
-        
-        <div>Paginas de los reportes</div>
-        
-    )
+
+export default  function Reportes() {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+      async function fetchData() {
+        const res = await fetch("/api/usuario", {
+            method: "GET", 
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await res.json()
+        setData(data)
+    }  
+    fetchData()
+}, [])
+
+if (!data) {
+    return <div>Cargando...</div>;
 }
+return (
+    <div>
+            {data.map((reporte, index) => (
+                <div key={index}>
+                    <p>Ubicacion: {reporte.rep_ubicacion}</p>
+                    <p>Descripcion:{reporte.rep_descripcion}</p>
+                    <p>Imagen: {reporte.rep_imagenURL}</p>
+                    <p>Alcaldia: {reporte.alcaldia.alc_nombre}</p>
+                  {/*   <p>Estado Reporte: {reporte.estado_reporte.edo_tipo}</p>
+                    
+                    <p>Nombre de reporte: {reporte.nombre_Usuario.nomu_nombre}</p> */}
+                </div>
+            ))}
+        </div>
+    );
+  
+}/* 
+return (
+    <div>
+        <p>Correo: {data.usu_correo}</p>
+        <p>Estado de cuenta: {data.usu_estado_cuenta ? 'Activa' : 'Inactiva'}</p>
+        <p>Nombre de usuario: {data.nombre_Usuario}</p>
+    </div>
+); */
--- src/app/api/consultas/route.js
+++ src/app/api/consultas/route.js
@@ -1,34 +1,58 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
+export async function GET( request) {
+    const alcaldias =await  prisma.alcaldia.findMany()
+
+    console.log(alcaldias) 
+    return NextResponse.json(alcaldias)
+}
+
+
+
+
+//YA NO EJECUTAR ESTA CONSULTA, YA QUE YA SE REGISTRO EN LA BD LAS ALCALDIAS
 export async function POST(request) {
 
     try {
-        const insertarDatosAlcaldia = await prisma.alcaldia.createMany({
+        const insertarDatosAlcaldia = await prisma.estado_reporte.createMany({
             data: [
-                {alc_id: "002", alc_nombre: "Azcapotzalco"},
-                {alc_id: "003", alc_nombre: "Coyoacán"},
-                {alc_id: "004", alc_nombre: "Cuajimalpa de Morelos"},
-                {alc_id: "005", alc_nombre: "Gustavo A. Madero"},
-                {alc_id: "006", alc_nombre: "Iztacalco"},
-                {alc_id: "007", alc_nombre: "Iztapalapa"},
-                {alc_id: "008", alc_nombre: "La Magdalena Contreras"},
-                {alc_id: "009", alc_nombre: "Milpa Alta"},
-                {alc_id: "010", alc_nombre: "Álvaro Obregón"},
-                {alc_id: "011", alc_nombre: "Tláhuac"},
-                {alc_id: "012", alc_nombre: "Tlalpan"},
-                {alc_id: "013", alc_nombre: "Xochimilco"},
-                {alc_id: "014", alc_nombre: "Benito Juárez"},
-                {alc_id: "015", alc_nombre: "Cuauhtémoc"},
-                {alc_id: "016", alc_nombre: "Miguel Hidalgo"},
-                {alc_id: "017", alc_nombre: "Venustiano Carranza"},
+                {alc_id: 2, alc_nombre: "Azcapotzalco"},
+                {alc_id: 3, alc_nombre: "Coyoacán"},
+                {alc_id: 4, alc_nombre: "Cuajimalpa de Morelos"},
+                {alc_id: 5, alc_nombre: "Gustavo A. Madero"},
+                {alc_id: 6, alc_nombre: "Iztacalco"},
+                {alc_id: 7, alc_nombre: "Iztapalapa"},
+                {alc_id: 8, alc_nombre: "La Magdalena Contreras"},
+                {alc_id: 9, alc_nombre: "Milpa Alta"},
+                {alc_id: 10, alc_nombre: "Álvaro Obregón"},
+                {alc_id: 11, alc_nombre: "Tláhuac"},
+                {alc_id: 12, alc_nombre: "Tlalpan"},
+                {alc_id: 13, alc_nombre: "Xochimilco"},
+                {alc_id: 14, alc_nombre: "Benito Juárez"},
+                {alc_id: 15, alc_nombre: "Cuauhtémoc"},
+                {alc_id: 16, alc_nombre: "Miguel Hidalgo"},
+                {alc_id: 17, alc_nombre: "Venustiano Carranza"},
             ]
         })
-        console.log(insertarDatosAlcaldia)
+        return  NextResponse.json(insertarDatosAlcaldia)
+
     } catch (error) {
         console.log(error.code)
         console.log(error.message)
 
     }
 
+}
+/* */
+
+
+export async function DELETE(request) {
+    try {
+        await prisma.usuario.deleteMany()
+        await prisma.nombre_Usuario.deleteMany()
+        return NextResponse.json("se borro exitosamente")
+    } catch (error) {
+        console.error("Hubo un error a la hora de querer eliminar los datos ", error)
+    }
 }
